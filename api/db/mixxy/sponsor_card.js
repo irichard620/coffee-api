c8d02655d38c7e93fbae075f6bf8dbd0cfe4e936
Ian Richard <ianr620@gmail.com>
2020-03-21 15:14:37 -0700

    Add sponsor stuff to sponsor card model

--- api/db/mixxy/sponsor_card.js
+++ api/db/mixxy/sponsor_card.js
@@ -8,6 +8,12 @@ function getSponsorCardDoc(cardModel) {
     dbDoc.card_id = cardModel.card_id;
   }
   dbDoc.sponsor_id = cardModel.sponsor_id || '';
+  dbDoc.sponsor_name = cardModel.sponsor_name || '';
+  dbDoc.sponsor_type = cardModel.sponsor_type || '';
+  dbDoc.hq_location = cardModel.hq_location || '';
+  dbDoc.website = cardModel.website || '';
+  dbDoc.about = cardModel.about || '';
+  dbDoc.logo_link = cardModel.logo_link || '';
   dbDoc.tags = cardModel.tags || [];
   dbDoc.card_image_link = cardModel.card_image_link || '';
   dbDoc.card_title = cardModel.card_title || '';
