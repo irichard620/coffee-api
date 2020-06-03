4928e935ba861ccbcb6a69d084e0df33a462375c
irichard620 <irichard@scu.edu>
2020-06-02 21:45:36 -0700

    Security round 1

--- api/controllers/mixxy/recipes.js
+++ api/controllers/mixxy/recipes.js
@@ -1,3 +1,6 @@
+const jwt = require('jsonwebtoken');
+const axios = require('axios').default;
+const uuidv4 = require('uuid/v4');
 const { getDb } = require('../../db/db');
 const getMixxyRecipeDoc = require('../../db/mixxy/recipe');
 
@@ -94,36 +97,64 @@ function getRecipeDetailsHandler(req, res) {
 function createSharedRecipeHandler(req, res) {
   const db = getDb();
   const collection = db.collection('mixxy_shared_recipes');
-  console.log(req.swagger.params.body.value)
-  const dbDoc = getMixxyRecipeDoc(req.swagger.params.body.value);
-  const recipeID = req.swagger.params.recipeID.value;
-  collection.findOne({ recipe_id: recipeID }, (err, item) => {
-    if (err) {
-      res.status(500);
-      res.json(err);
-    } else if (!item) {
-      collection.insertOne(dbDoc, (err2) => {
-        if (err2) {
+  // Check with apple about valid device
+  const jwtToken = jwt.sign(
+    { iss: 'K78F8G9J87', iat: Math.floor(new Date() / 1000) },
+    process.env.APPLE_PRIVATE_KEY,
+    { algorithm: 'ES256', header: { kid: process.env.APPLE_KEY_ID } }
+  )
+  console.log(jwtToken)
+  const instance = axios.create({
+    baseURL: 'https://api.development.devicecheck.apple.com',
+    timeout: 1000,
+    headers: {'Authorization': `Bearer ${jwtToken}`}
+  });
+  instance.post('', {
+      device_token: req.swagger.params.body.value.device_token,
+      transaction_id: uuidv4(),
+      timestamp: new Date(),
+    })
+    .then(function (response) {
+      console.log(response)
+      if (response.status !== 200) {
+        res.status(500);
+        res.json('Invalid device');
+      }
+      // Get db doc
+      const dbDoc = getMixxyRecipeDoc(req.swagger.params.body.value);
+      const recipeID = req.swagger.params.recipeID.value;
+      collection.findOne({ recipe_id: recipeID }, (err, item) => {
+        if (err) {
           res.status(500);
           res.json(err);
+        } else if (!item) {
+          collection.insertOne(dbDoc, (err2) => {
+            if (err2) {
+              res.status(500);
+              res.json(err);
+            } else {
+              res.status(201);
+              res.json(`https://mixxy.page.link/?link=https://mixxyapp.com/${dbDoc.recipe_id}&ibi=com.IanRichard.Mixxy`);
+            }
+          });
         } else {
-          res.status(201);
-          res.json(`https://mixxy.page.link/?link=https://mixxyapp.com/${dbDoc.recipe_id}&ibi=com.IanRichard.Mixxy`);
+          const newValues = { $set: dbDoc };
+          collection.updateOne({ recipe_id: recipeID }, newValues, (err2, dbRes) => {
+            if (err2) {
+              res.status(500);
+              res.json(err2);
+            } else {
+              res.status(201);
+              res.json(`https://mixxy.page.link/?link=https://mixxyapp.com/${item.recipe_id}&ibi=com.IanRichard.Mixxy`);
+            }
+          });
         }
       });
-    } else {
-      const newValues = { $set: dbDoc };
-      collection.updateOne({ recipe_id: recipeID }, newValues, (err2, dbRes) => {
-        if (err2) {
-          res.status(500);
-          res.json(err2);
-        } else {
-          res.status(201);
-          res.json(`https://mixxy.page.link/?link=https://mixxyapp.com/${item.recipe_id}&ibi=com.IanRichard.Mixxy`);
-        }
-      });
-    }
-  });
+    })
+    .catch(function (error) {
+      res.status(500);
+      res.json(error);
+    });
 }
 
 module.exports = {
